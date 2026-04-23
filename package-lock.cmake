570871d3aaa1e1edb929c165a6615d1fca9ac07b
Aleksandr Dobkin <alex@dobk.in>
2026-04-22 18:40:50 -0700

    update

---
+++ package-lock.cmake
@@ -0,0 +1,10 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# baselib
+CPMDeclarePackage(baselib
+  NAME baselib
+  VERSION 0.0.0
+  GIT_TAG c6ea2af91ee819a343e37c8f122ec350c065bb96
+  GITHUB_REPOSITORY adob/baselib
+)
